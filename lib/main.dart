f35f616f56ca3305f9a1fff8ed38760fb4c23c0c
Omid-mohebi <omohebi12@gmail.com>
2021-02-23 15:36:31 +0430

    fix: remove some code

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_icons/flutter_icons.dart';
-
 import 'package:get/get.dart';
-import 'package:marcopolo/app/modules/bookMark/views/book_mark_view.dart';
 import 'package:marcopolo/app/modules/home/bindings/home_binding.dart';
-
-import 'app/modules/home/views/home_view.dart';
-import 'app/modules/profile/views/profile_view.dart';
-import 'app/modules/tabHome/views/circleIndicator.dart';
 import 'app/routes/app_pages.dart';
-import 'app/theme/AppColors.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -31,116 +23,116 @@ void main() {
       initialRoute: AppPages.INITIAL,
       initialBinding: HomeBinding(),
       getPages: AppPages.routes,
-      builder: (context, child) {
-        return Scaffold(
-          body: child,
-          bottomNavigationBar: Obx(() => Container(
-                height: showBNB.value ? 90 : 0,
-                color: Colors.white,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 50),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                        icon: Icon(
-                          Foundation.home,
-                          size: 30,
-                        ),
-                        onPressed: () {
-                          print('1111');
-                        },
-                      ),
-                      IconButton(
-                        icon: Icon(
-                          Foundation.bookmark,
-                          size: 30,
-                        ),
-                        onPressed: () {},
-                      ),
-                      IconButton(
-                        icon: Icon(
-                          Ionicons.md_person,
-                          size: 30,
-                        ),
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                ),
-              )),
-        );
-        // return DefaultTabController(
-        //   length: 3,
-        //   child: Stack(
-        //     alignment: Alignment.bottomCenter,
-        //     children: [
-        // Padding(
-        //   padding: const EdgeInsets.only(bottom: 50.0),
-        //   child: child,
-        // ),
-        // GestureDetector(
-        //   onTap: () {
-        // return Navigator.of(Get.context).push(
-        //   MaterialPageRoute(
-        //     builder: (BuildContext context) => BookMarkView(),
-        //   ),
-        // );
-        //   Get.toNamed(Routes.PROFILE);
-        // },
-        // child: Padding(
-        //   padding: const EdgeInsets.symmetric(vertical: 15),
-        //   child: TabBar(
-        //     labelColor: Colors.black,
-        //     unselectedLabelColor: AppColors.decative,
-        //     indicatorSize: TabBarIndicatorSize.label,
-        //     indicatorColor: AppColors.pink,
-        //     // indicatorWeight: 3,
-        //     indicator: RoundUnderlineTabIndicator(
-        //       borderSide: BorderSide(
-        //         width: 3,
-        //         color: AppColors.pink,
-        //       ),
-        //     ),
-        //     tabs: [
-        //       //TODO add khaled icon
-        //       Padding(
-        //         padding: const EdgeInsets.only(bottom: 12),
-        //         child: Icon(
-        //           Foundation.home,
-        //           size: 30,
-        //         ),
-        //       ),
-        //       //TODO add khaled icon
-        //       Padding(
-        //         padding: const EdgeInsets.only(bottom: 12),
-        //         child: Icon(
-        //           Foundation.bookmark,
-        //           size: 30,
-        //           // color: controller.currentTab.value == 0
-        //           //     ? AppColors.darkBlue
-        //           //     : AppColors.chartBlue,
-        //         ),
-        //       ),
-        //       //TODO add khaled icon
-        //       Padding(
-        //         padding: const EdgeInsets.only(bottom: 12),
-        //         child: Icon(
-        //           Ionicons.md_person,
-        //           size: 30,
-        //           // color: controller.currentTab.value == 0
-        //           //     ? AppColors.darkBlue
-        //           //     : AppColors.chartBlue,
-        //         ),
-        //       ),
-        //     ],
-        //   ),
-        // ),
-        //         )
-        //       ],
-        //     ),
-        //   );
-      },
+      // builder: (context, child) {
+      //   return Scaffold(
+      //     body: child,
+      //     bottomNavigationBar: Obx(() => Container(
+      //           height: showBNB.value ? 90 : 0,
+      //           color: Colors.white,
+      //           child: Padding(
+      //             padding: const EdgeInsets.symmetric(horizontal: 50),
+      //             child: Row(
+      //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //               children: [
+      //                 IconButton(
+      //                   icon: Icon(
+      //                     Foundation.home,
+      //                     size: 30,
+      //                   ),
+      //                   onPressed: () {
+      //                     print('1111');
+      //                   },
+      //                 ),
+      //                 IconButton(
+      //                   icon: Icon(
+      //                     Foundation.bookmark,
+      //                     size: 30,
+      //                   ),
+      //                   onPressed: () {},
+      //                 ),
+      //                 IconButton(
+      //                   icon: Icon(
+      //                     Ionicons.md_person,
+      //                     size: 30,
+      //                   ),
+      //                   onPressed: () {},
+      //                 ),
+      //               ],
+      //             ),
+      //           ),
+      //         )),
+      //   );
+      // return DefaultTabController(
+      //   length: 3,
+      //   child: Stack(
+      //     alignment: Alignment.bottomCenter,
+      //     children: [
+      // Padding(
+      //   padding: const EdgeInsets.only(bottom: 50.0),
+      //   child: child,
+      // ),
+      // GestureDetector(
+      //   onTap: () {
+      // return Navigator.of(Get.context).push(
+      //   MaterialPageRoute(
+      //     builder: (BuildContext context) => BookMarkView(),
+      //   ),
+      // );
+      //   Get.toNamed(Routes.PROFILE);
+      // },
+      // child: Padding(
+      //   padding: const EdgeInsets.symmetric(vertical: 15),
+      //   child: TabBar(
+      //     labelColor: Colors.black,
+      //     unselectedLabelColor: AppColors.decative,
+      //     indicatorSize: TabBarIndicatorSize.label,
+      //     indicatorColor: AppColors.pink,
+      //     // indicatorWeight: 3,
+      //     indicator: RoundUnderlineTabIndicator(
+      //       borderSide: BorderSide(
+      //         width: 3,
+      //         color: AppColors.pink,
+      //       ),
+      //     ),
+      //     tabs: [
+      //       //TODO add khaled icon
+      //       Padding(
+      //         padding: const EdgeInsets.only(bottom: 12),
+      //         child: Icon(
+      //           Foundation.home,
+      //           size: 30,
+      //         ),
+      //       ),
+      //       //TODO add khaled icon
+      //       Padding(
+      //         padding: const EdgeInsets.only(bottom: 12),
+      //         child: Icon(
+      //           Foundation.bookmark,
+      //           size: 30,
+      //           // color: controller.currentTab.value == 0
+      //           //     ? AppColors.darkBlue
+      //           //     : AppColors.chartBlue,
+      //         ),
+      //       ),
+      //       //TODO add khaled icon
+      //       Padding(
+      //         padding: const EdgeInsets.only(bottom: 12),
+      //         child: Icon(
+      //           Ionicons.md_person,
+      //           size: 30,
+      //           // color: controller.currentTab.value == 0
+      //           //     ? AppColors.darkBlue
+      //           //     : AppColors.chartBlue,
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      //         )
+      //       ],
+      //     ),
+      //   );
+      // },
     ),
   );
 }
