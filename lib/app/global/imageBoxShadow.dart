83c2ae26de42fdf4d7a7984f6c05827af2f6a465
Omid-mohebi <omohebi12@gmail.com>
2021-06-14 14:30:23 +0430

    update: image box shadow widget

--- lib/app/global/imageBoxShadow.dart
+++ lib/app/global/imageBoxShadow.dart
@@ -10,29 +10,39 @@ class ImageBoxShadow extends StatelessWidget {
   final String imgUrl;
   final String title;
   final String myTag;
+  final bool notTheSecondPage;
+  final VoidCallback ontap;
   // final AnimatedHomeController homeController;
   // final Function goto;
-  ImageBoxShadow({this.imgUrl, this.title, this.myTag});
+  ImageBoxShadow(
+      {this.imgUrl,
+      this.title,
+      this.myTag,
+      this.notTheSecondPage = false,
+      this.ontap});
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () async {
         // homeController.secondPage.value = true;
-        Get.find<AnimatedHomeController>().selectedImg = imgUrl;
-        Get.find<AnimatedHomeController>().selectedText = title;
-        Get.find<AnimatedHomeController>().heroTag.value = myTag;
-        Get.find<AnimatedHomeController>().controller.forward();
-        Timer(
-          Duration(milliseconds: 100),
-          () => {
-            Get.find<HomeController>().coastController.animateTo(
-                beach: 1,
-                duration: Duration(milliseconds: 400),
-                curve: Curves.easeInOut)
-          },
-        );
-
-        // print(Get.find<AnimatedHomeController>().heroTag);
+        if (notTheSecondPage == false) {
+          Get.find<AnimatedHomeController>().selectedImg = imgUrl;
+          Get.find<AnimatedHomeController>().selectedText = title;
+          Get.find<AnimatedHomeController>().heroTag.value = myTag;
+          Get.find<AnimatedHomeController>().controller.forward();
+          Timer(
+            Duration(milliseconds: 1),
+            () {
+              Get.find<AnimatedHomeController>().secondPage.value = true;
+              Get.find<HomeController>().coastController.animateTo(
+                  beach: 1,
+                  duration: Duration(milliseconds: 400),
+                  curve: Curves.easeInOut);
+            },
+          );
+        } else {
+          ontap();
+        }
       },
       child: Stack(
         children: [
@@ -40,7 +50,7 @@ class ImageBoxShadow extends StatelessWidget {
             height: double.infinity,
             child: Image.asset(
               imgUrl,
-              fit: BoxFit.fitHeight,
+              fit: BoxFit.cover,
             ),
           ),
           Container(
@@ -63,7 +73,7 @@ class ImageBoxShadow extends StatelessWidget {
             left: 10,
             child: Text(
               title,
-              style: AppTextTheme.normalBText(),
+              style: AppTextTheme.normalBText().copyWith(color: Colors.white),
             ),
           )
         ],
