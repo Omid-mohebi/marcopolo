f9871cc2158db8a56f431887bccf4777bd8774ad
Omid-mohebi <omohebi12@gmail.com>
2021-06-14 14:30:59 +0430

    update: update book mark page

--- lib/app/modules/bookMark/controllers/book_mark_controller.dart
+++ lib/app/modules/bookMark/controllers/book_mark_controller.dart
@@ -1,12 +1,24 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:marcopolo/app/modules/bookMark/views/comming_trips_view.dart';
+import 'package:marcopolo/app/modules/bookMark/views/post_trips_view.dart';
 
 class BookMarkController extends GetxController {
-  //TODO: Implement BookMarkController
-
+  var isSelected = true.obs;
+  List<Widget> screens;
+  PageController pageController = PageController(
+      // initialPage: isSelected.value ? 0 : 1,
+      );
+  Curve myEaseOutBack = Cubic(0.175, 0.885, 0.39, 1.1);
   final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+
+    screens = [
+      CommingTripsView(),
+      PostTripsView(),                
+    ];
   }
 
   @override
--- lib/app/modules/bookMark/views/book_mark_view.dart
+++ lib/app/modules/bookMark/views/book_mark_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:marcopolo/app/theme/AppColors.dart';
 
 import '../controllers/book_mark_controller.dart';
 
@@ -8,15 +9,85 @@ class BookMarkView extends GetView<BookMarkController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BookMarkView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'BookMarkView is working',
-          style: TextStyle(fontSize: 20),
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(180),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Row(
+            children: [
+              Expanded(
+                child: Obx(
+                  () => GestureDetector(
+                    onTap: () {
+                      controller.isSelected.value = true;
+                      controller.pageController.animateToPage(0,
+                          duration: Duration(milliseconds: 350),
+                          curve: controller.myEaseOutBack);
+                    },
+                    child: Container(
+                      height: 50,
+                      color: controller.isSelected.value == true
+                          ? Colors.black
+                          : AppColors.weakDecative,
+                      child: Center(
+                        child: Text(
+                          'Comming Trips',
+                          style: TextStyle(
+                            color: controller.isSelected.value == true
+                                ? Colors.white
+                                : AppColors.strongDecative,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Obx(
+                  () => GestureDetector(
+                    onTap: () {
+                      controller.isSelected.value = false;
+                      controller.pageController.animateToPage(1,
+                          duration: Duration(milliseconds: 350),
+                          curve: controller.myEaseOutBack);
+                    },
+                    child: Container(
+                      height: 50,
+                      color: controller.isSelected.value == false
+                          ? Colors.black
+                          : AppColors.weakDecative,
+                      child: Center(
+                        child: Text(
+                          'Post Trips',
+                          style: TextStyle(
+                            color: controller.isSelected.value == false
+                                ? Colors.white
+                                : AppColors.strongDecative,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
+      ),
+      body: PageView(
+        
+        onPageChanged: (ind) {
+          if (ind == 0) {
+            controller.isSelected.value = true;
+            return;
+          }
+          controller.isSelected.value = false;
+        },
+        controller: controller.pageController,
+        children: controller.screens,
+        physics: BouncingScrollPhysics(),
       ),
     );
   }
