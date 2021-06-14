685e905d97630205d3f68140d4a110ccc6f1cff0
Omid-mohebi <omohebi12@gmail.com>
2021-06-14 14:33:21 +0430

    update: update profile page

--- lib/app/modules/profile/controllers/profile_controller.dart
+++ lib/app/modules/profile/controllers/profile_controller.dart
@@ -1,17 +1,40 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:marcopolo/app/controller/auth_controller.dart';
+import 'package:marcopolo/app/modules/login/views/login_view.dart';
+import 'package:marcopolo/app/modules/profileSetting/controllers/profile_setting_controller.dart';
+import 'package:marcopolo/app/modules/profileSetting/views/profile_setting_view.dart';
+import 'package:marcopolo/app/modules/profileSettingNotLogin/views/profile_setting_not_login_view.dart';
 
-class ProfileController extends GetxController {
-  //TODO: Implement ProfileController
-
+class ProfileController extends GetxController
+    with SingleGetTickerProviderMixin {
+  List<Widget> screens;
   final count = 0.obs;
+  PageController pageController;
+  AnimationController controller;
   @override
-  void onInit() {
+  void onInit() async {
+    // get.hashCode
     super.onInit();
+
+    pageController = PageController(
+        initialPage:
+            await AuthController.to.isUserSignedInedToFirebase() ? 2 : 1);
+    screens = [
+      LoginView(),
+      ProfileSettingNotLoginView(),
+      ProfileSettingView(),
+    ];
+    Get.put(ProfileSettingController());
   }
 
   @override
   void onReady() {
     super.onReady();
+    controller = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
   }
 
   @override
